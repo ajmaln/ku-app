e51341b4bbd53cf13435cb4108dfea00fde4ccc0
Ajmal Noushad <ajufhn@gmail.com>
2018-09-08 00:05:04 +0530

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -37,18 +37,6 @@ function setTokenSentToServer(val) {
 
 messaging.requestPermission().then(function () {
     console.log('Notification permission granted.');
-    messaging.getToken().then(function (currentToken) {
-        if (currentToken) {
-            sendTokenToServer(currentToken);
-            //updateUIForPushEnabled(currentToken);
-            console.log('Token generated', currentToken)
-        } else {
-            // Show permission request.
-            console.log('No Instance ID token available. Request permission to generate one.');
-            // Show permission UI.
-            //updateUIForPushPermissionRequired();
-            setTokenSentToServer(false);
-        }
     }).catch(function (err) {
         console.log('An error occurred while retrieving token. ', err);
         //showToken('Error retrieving Instance ID token. ', err);
