952cc63ba73ea9943ec4af216b7e076c9687ad86
Ajmal Noushad <ajufhn@gmail.com>
2018-09-08 00:00:21 +0530

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -15,7 +15,6 @@ firebase.initializeApp({
 // messages.
 const messaging = firebase.messaging();
 
-messaging.usePublicVapidKey("BDDNlVdwUP39dJEu9rfD7kBEOUvaUXhJkuQqC4Nc4B0P3HtpVl0E_9EXjgCqYp0N0UvO3pqDvCoE8qzoEUdRo_U");
 
 
 messaging.setBackgroundMessageHandler(function(payload) {
@@ -32,89 +31,3 @@ messaging.setBackgroundMessageHandler(function(payload) {
 });
 
 
-function sendTokenToServer(tooken) {
-
-}
-
-function setTokenSentToServer(val) {
-
-}
-
-
-messaging.requestPermission().then(function () {
-    console.log('Notification permission granted.');
-    messaging.getToken().then(function (currentToken) {
-        if (currentToken) {
-            sendTokenToServer(currentToken);
-            //updateUIForPushEnabled(currentToken);
-            console.log('Token generated', currentToken)
-        } else {
-            // Show permission request.
-            console.log('No Instance ID token available. Request permission to generate one.');
-            // Show permission UI.
-            //updateUIForPushPermissionRequired();
-            setTokenSentToServer(false);
-        }
-    }).catch(function (err) {
-        console.log('An error occurred while retrieving token. ', err);
-        //showToken('Error retrieving Instance ID token. ', err);
-        setTokenSentToServer(false);
-    });
-}).catch(function (err) {
-    console.log('Unable to get permission to notify.', err);
-});
-
-messaging.onTokenRefresh(function () {
-    messaging.getToken().then(function (refreshedToken) {
-        console.log('Token refreshed.');
-        // Indicate that the new Instance ID token has not yet been sent to the
-        // app server.
-        setTokenSentToServer(false);
-        // Send Instance ID token to app server.
-        sendTokenToServer(refreshedToken);
-        // ...
-    }).catch(function (err) {
-        console.log('Unable to retrieve refreshed token ', err);
-        //showToken('Unable to retrieve refreshed token ', err);
-    });
-});
-
-messaging.onTokenRefresh(function () {
-    messaging.getToken().then(function (refreshedToken) {
-        console.log('Token refreshed.');
-        // Indicate that the new Instance ID token has not yet been sent to the
-        // app server.
-        setTokenSentToServer(false);
-        // Send Instance ID token to app server.
-        sendTokenToServer(refreshedToken);
-        // ...
-    }).catch(function (err) {
-        console.log('Unable to retrieve refreshed token ', err);
-        //showToken('Unable to retrieve refreshed token ', err);
-    });
-});
-
-messaging.onMessage(function (payload) {
-    console.log('Message received. ', payload);
-    var notificationTitle = "New Updates!";
-    var notificationOptions = {
-        body: payload.message,
-        icon: logo
-    };
-
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
-
-messaging.setBackgroundMessageHandler(function (payload) {
-    console.log('Received background message ', payload);
-    // Customize notification here
-    var notificationTitle = "New Updates!";
-    var notificationOptions = {
-        body: payload.message,
-        icon: logo
-    };
-
-    return self.registration.showNotification(notificationTitle,
-        notificationOptions);
-});
